578608ff6fff71463008cc0a8ab02e8cec093fa9
ale88andr <ale88andr@gmail.com>
2015-04-08 12:47:06 +0300

    [REFACTOR](Core:App) Refactoring methods, add comments

--- app/core/App.php
+++ app/core/App.php
@@ -1,36 +1,29 @@
 <?php
 class App
 {
-    protected $controller;
+    protected   $controller, 
+                $action,
+                $params = [];
 
-    protected $action;
-
-    protected $params = [];
-
+    /**
+    * Application html layout name
+    *
+    * @var string
+    */
     static private $_layout;
 
+    /**
+    * Controller generated content
+    *
+    * @var string
+    */
     public $content;
 
     function __construct(){
-        $url = $this->parseURL();
-
-        if(file_exists(ROOT_APP . 'controllers' . DS . $url[0] . EXT)){
-            $this->controller = array_shift($url);
-            require_once ROOT_APP . 'controllers' . DS . $this->controller . EXT;
-            $this->controller = new $this->controller;
-
-            $this->action = (empty($url[0])) ? 'index' : array_shift($url);
-
-            if(method_exists($this->controller, $this->action)){
-                $this->params = $url;
-                ob_start();
-                call_user_func_array([$this->controller, $this->action], $this->params);
-                $this->content = ob_get_clean();
-            } else {
-                echo 'Controller "' . get_class($this->controller) . '" does\'t have action "' . $this->action . '"';
-            }
-        } else {
-            echo 'Controller "' . $url[0] . '" not found. Searched in: "' . ROOT_APP . 'controllers' . DS . '"';
+        try{
+            $this->applyController($this->parseURL());
+        } catch(Exception $e) {
+            die($e->getMessage());
         }
 
         if(empty(self::$_layout)) {
@@ -38,14 +31,70 @@ class App
         }
     }
 
+    /**
+    * Controller handler by request params.
+    *
+    * @param array $url Request string (URI)
+    * @return void
+    */
+    private function applyController($url) {
+        $this->controller = array_shift($url);
+        $controller_dir = ROOT_APP . 'controllers' . DS;
+        $controller_path = $controller_dir . $this->controller . EXT;
+        if(file_exists($controller_path)){
+            require_once $controller_path;
+            $this->controller = new $this->controller;
+
+            $this->action = (empty($url[0])) ? 'index' : array_shift($url);
+
+            $this->applyControllerAction($url);
+        } else {
+            throw new Exception("Controller '{$this->controller}' not found. Searched in: '{$controller_dir}' <br>
+                Search path: {$controller_path}");
+        }
+    }
+
+    /**
+    * Action handler by request params.
+    *
+    * @param array $url Part of request URI
+    * @return void
+    */
+    private function applyControllerAction($url) {
+        if(method_exists($this->controller, $this->action)){
+            $this->params = $url;
+            ob_start();
+            call_user_func_array([$this->controller, $this->action], $this->params);
+            $this->content = ob_get_clean();
+        } else {
+            throw new Exception("Controller '" . get_class($this->controller) . "' does't have action '{$this->action}'");
+        }
+    }
+
+    /**
+    * Set application html layout.
+    *
+    * @param string $layout HTML layout name
+    * @return void
+    */
     public static function setLayout($layout = false) {
         self::$_layout = ($layout) ? $layout : 'application';
     }
 
+    /**
+    * Get application html layout.
+    *
+    * @return srting
+    */
     private function getLayout() {
         return self::$_layout;
     }
 
+    /**
+    * Apply application html layout.
+    *
+    * @return string
+    */
     public function layout() {
         $layout_dir = ROOT_APP . 'views' . DS . 'layouts' . DS;
         $layout_file = self::getLayout() . EXT_VIEW;
@@ -58,6 +107,11 @@ class App
         }
     }
 
+    /**
+    * Parse request URI
+    *
+    * @return array
+    */
     function parseURL(){
         if(isset($_GET['url'])){
             return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
