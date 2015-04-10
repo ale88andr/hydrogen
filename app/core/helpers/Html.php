1507cb34e74f5c72ebbb2b0babc8548872ea183e
ale88andr <ale88andr@gmail.com>
2015-04-10 11:39:53 +0300

    [UPD](Core:Helpers) update Form helper(add/fix helpers), add comments

--- app/core/helpers/Html.php
+++ app/core/helpers/Html.php
@@ -4,11 +4,24 @@
 
 class Html
 {
-    public static $voidTags = [
-        'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input',
-        'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'
-        ];
+    /**
+     * Html void elements.
+     *
+     * @var array
+     */
+    public static $voidTags = [ 'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input',
+                                'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr' ];
 
+    /**
+     * Method return html element.
+     * Example: Html::tag('div', 'Div content', ['class' => 'div_tag'])
+     * Return: <div class='div_tag'>Div content</div>
+     *
+     * @param string $tag       Tag name string
+     * @param string $content   Tag content
+     * @param array $options    Html elements attributes
+     * @return string (html)
+     */
     public static function tag($tag, $content = false, $options = [])
     {
         $html = "<{$tag} " . static::optionsHandler($options) . '>';
@@ -19,14 +32,20 @@ class Html
         return $html . $content . '</' . $tag . '>';
     }
 
+    /**
+     * Parse html attributes for elements.
+     *
+     * @param array $options    Html elements attributes
+     * @return string
+     */
     public static function optionsHandler($options = [])
     {
         if(is_array($options)) {
             $html_params = '';
             foreach ($options as $tag_attribute => $attribute_value) {
                 if(!empty($attribute_value)) {
-                    $html_params .= is_bool($attribute_value) ? "{$tag_attribute} "
-                                                              : "{$tag_attribute}={$attribute_value} ";
+                    $html_params .= (is_bool($attribute_value)) ? "{$tag_attribute} "
+                                                                : "{$tag_attribute}={$attribute_value} ";
                 }
             }
 
--- app/core/helpers/Link.php
+++ app/core/helpers/Link.php
@@ -7,10 +7,18 @@
 */
 class Link extends Html
 {
-
+    /**
+     * Method return html a tag.
+     * Example: Link::to('home', 'Home page', ['class' => 'link'])
+     * Return: <a href='www.site.domain/home' class='link'>Home page</a>
+     *
+     * @param string $href      URI string
+     * @param string $title     Link title
+     * @param array $options    Html elements attributes
+     * @return string (html)
+     */
     public static function to($href, $title, $options = [])
     {
-        $href = ['href' => ROOT_URL . $href];
-        return static::tag('a', $title, array_merge($href, $options));
+        return static::tag('a', $title, array_merge(['href' => ROOT_URL . $href], $options));
     }
 }
